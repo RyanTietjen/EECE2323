0707f8ed03ca89b8385aafbcabf1298ac6387b5a
RyanTietjen <36613081+RyanTietjen@users.noreply.github.com>
2024-02-09 11:17:31 -0500

    Create mux.sv

---
+++ lab3/mux.sv
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/09/2024 09:31:04 AM
+// Design Name: 
+// Module Name: mux
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mux(
+    input [15:0] a,
+    input [15:0] b,
+    input sel_mux,
+    output [15:0] out
+    );
+    
+    assign out = sel_mux ? a : b;
+endmodule
