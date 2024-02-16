adf257e1cbb520cdb7896942dc7b64c2e912e954
Ethylburner <79605559+Ethylburner@users.noreply.github.com>
2024-02-15 20:40:46 -0500

    Update fulladder_tb.sv

--- fulladder_tb.sv
+++ fulladder_tb.sv
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 01/19/2024 10:02:18 AM
+// Create Date: 02/09/2024 10:00:02 AM
 // Design Name: 
-// Module Name: fulladder_tb
+// Module Name: alu_reg_file_tb
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -19,22 +19,54 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-`timescale 1ns / 1ps
-module adder_tb();
-    logic a;
-    logic b;
-    logic c_in;
-    logic sum;
-    logic c_out;
-    full_adder_wrapper UTT(.a(a),.b(b),.c_in(c_in),.sum(sum),.c_out(c_out));
-    initial begin
-        a = 1'b0; b=1'b0; c_in=1'b0;
-        #100 a=1'b1; b=1'b0; c_in= 1'b0;
-        #100 a=1'b0; b=1'b1; c_in= 1'b0;
-        #100 a=1'b0; b=1'b0; c_in= 1'b1;
-        #100 a=1'b1; b=1'b1; c_in= 1'b0;
-        #100 a=1'b1; b=1'b0; c_in= 1'b1;
-        #100 a=1'b0; b=1'b1; c_in= 1'b1;
-        #100 a=1'b1; b=1'b1; c_in= 1'b1;
+
+module alu_reg_file_tb();
+    //inputs
+    logic reset;
+    logic clk;
+    logic [2:0] rd0_addr;
+    logic [2:0] rd1_addr;
+    logic [2:0] wr_addr;
+    logic [15:0] wr_data;
+    logic RegWrite;
+    logic [15:0] alu_input2_instr_src;
+    logic ALUSrc2;
+    logic ALUSrc1;
+    logic[2:0] ALUOp;
+    //outputs
+    logic[15:0] result;
+    logic [15:0] input1;
+    logic [15:0] input2;
+    logic take_branch;
+    logic ovf;
+    //between wires
+    logic[15:0] ReadData1;
+    logic[15:0] ReadData2;
+    
+reg_file register1(reset, clk, RegWrite, rd0_addr, rd1_addr, wr_addr, wr_data, ReadData1, ReadData2);
+mux mux1(ReadData1, 15'b0, ALUSrc1, input1);
+mux mux2(ReadData2, alu_input2_instr_src, ALUSrc2, input2);
+alu sixteenbit_alu(input1, input2, ALUOp, result, take_branch, ovf);
+
+//sixteenbit_alu UTT(.a(input1), .b(input2), .s(ALUOp), .f(result), .take_branch(take_branch), .ovf(ovf));
+//mux1 UTT(.a(ReadData1),.b(15'b0), .sel_mux(ALUSrc1), .out(input1));
+//mux2 UTT(.a(ReadData2),.b(alu_input2_instr_src), .sel_mux(ALUSrc2), .out(input2));
+//register1 UTT(.rst(reset), .clk(clk), .wr_en(RegWrite), .rd0_addr(rd0_addr), .rd1_addr(rd1_addr), .wr_addr(wr_addr), .wr_data(wr_data), .rd0_data(ReadData1), .rd1_data(ReadData2));
+initial begin
+    reset=1;
+    #10 reset = 0;
     end
+    
+initial begin
+    clk=1;
+    forever #5 clk=~clk;
+end
+initial begin
+    //ALUsrc1 = 1: zero register, 0: ReadData1
+    //ALUsrc2 = 1: alu_input2_instr_src, 0: ReadData2
+   
+    ALUSrc1 = 1; ALUSrc2 = 1; alu_input2_instr_src = 16'hC; ALUOp = 0; RegWrite = 0; 
+    #100;
+end
+
 endmodule
