8e01a6a9453b0b2a10e3896441e66caaaaeed9fa
RyanTietjen <36613081+RyanTietjen@users.noreply.github.com>
2024-03-29 11:10:39 -0400

    Create program_counter.sv

---
+++ lab7/program_counter.sv
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/22/2024 09:46:15 AM
+// Design Name: 
+// Module Name: program_counter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module program_counter(
+    input clk,
+    input rst,
+    output logic [8:0] pc,
+    input logic alu_take_branch,
+    input logic offset[8:0]
+    );
+    
+    always @ (posedge clk, posedge rst) begin
+        if (rst) begin
+            pc <= 0;
+        end
+        else begin
+            pc <= alu_take_branch ? pc + offset : pc + 1;
+        end
+    end
+    
+        
+       
+    
+endmodule
